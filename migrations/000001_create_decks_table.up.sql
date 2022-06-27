35aecee3c0036e91fae22bcc6851192db6412dee
Sheldon Chi <sheldonchi@gmail.com>
2022-06-27 22:30:31 +0800

    Fix db migration: varchar(2)[] -> varchar(3)[] for cards

--- migrations/000001_create_decks_table.up.sql
+++ migrations/000001_create_decks_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS decks (
   id uuid DEFAULT uuid_generate_v4 (),
   shuffled boolean,
-  cards varchar(2)[],
+  cards varchar(3)[],
   created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
   version integer NOT NULL DEFAULT 1,
   PRIMARY KEY (id)
